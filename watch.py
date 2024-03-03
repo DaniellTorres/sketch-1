917abba85c12ca0d7bb3c6ef62c145a4ab837672
Daniel Torres <53536633+DanielLOTorres@users.noreply.github.com>
2024-03-02 21:43:00 -0300

    Add files via upload

---
+++ watch.py
@@ -0,0 +1,22 @@
+import os
+import time
+import requests
+import json
+
+def monitor_folder(folder_path, server_url):
+    while True:
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.json'):
+                file_path = os.path.join(folder_path, filename)
+                with open(file_path, 'r') as file:
+                    data = json.load(file)
+                    requests.post(server_url, json=data)
+                    print("Sent", data)
+        time.sleep(2)  
+
+monitor_folder('C:/Users/Daniel/Documents/Projetos/elipse/prServer/', 'http://localhost:3000/upload')
+
+
+
+
+
